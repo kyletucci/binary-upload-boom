cfdbfcf865b009c2af078ef3dccd54cc723ec44f
castthecalmingapple <kyletucci@gmail.com>
2023-12-19 22:49:55 -0500

    Fixed error with header view

--- controllers/sops.js
+++ controllers/sops.js
@@ -1,46 +1,46 @@
 const Sop = require("../models/Sop");
 
 module.exports = {
-    getSops: async (req, res) => {
-      try {
-        const sops = await Sop.find().lean();
-        res.render("sop.ejs", { sops: sops, user: req.user });
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    displaySop: async (req, res) => {
-      try {
-        const sops = await Sop.find({ _id: req.params.id});
-        res.render("displaySop.ejs", { sops: sops, user: req.user });
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    createSop: async (req, res) => {
-      try {
-        await Sop.create({
-          submittedBy: req.user.userName,
-          title: req.body.title,
-          objective: req.body.objective,
-          prerequisites: req.body.prerequisites,
-          procedure: req.body.procedure,
-          conclusion: req.body.conclusion,
-        });
-        console.log("Sop has been added!");
-        res.redirect("/sops");
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    deleteSop: async (req, res) => {
-      try {
-        // Delete post from db
-        await Sop.remove({ _id: req.params.id });
-        console.log("Deleted Sop");
-        res.redirect("/sops");
-      } catch (err) {
-        res.redirect("/sops");
-      }
-    },
-  };
+  getSops: async (req, res) => {
+    try {
+      const sops = await Sop.find().lean();
+      res.render("sop.ejs", { sops: sops, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  displaySop: async (req, res) => {
+    try {
+      const sops = await Sop.find({ _id: req.params.id });
+      res.render("displaySop.ejs", { sops: sops, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createSop: async (req, res) => {
+    try {
+      await Sop.create({
+        submittedBy: req.user.userName,
+        title: req.body.title,
+        objective: req.body.objective,
+        prerequisites: req.body.prerequisites,
+        procedure: req.body.procedure,
+        conclusion: req.body.conclusion,
+      });
+      console.log("Sop has been added!");
+      res.redirect("/sops");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteSop: async (req, res) => {
+    try {
+      // Delete post from db
+      await Sop.remove({ _id: req.params.id });
+      console.log("Deleted Sop");
+      res.redirect("/sops");
+    } catch (err) {
+      res.redirect("/sops");
+    }
+  },
+};
